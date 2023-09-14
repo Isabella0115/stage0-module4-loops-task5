b7849f00a39542e533ab8398b6e788d38343f68b
Isabella Karibian <isabellakaribian@gmail.com>
2023-09-14 15:15:49 +0400

    m4LoopT5

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,31 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        int i, j, k;
+        for (i= 0; i<= height-1 ; i++)
+        {
+            for (j=0; j<i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (k=i; k<=height-1; k++)
+            {
+                System.out.print("8" + " ");
+            }
+            System.out.println(" ");
+        }
+        for (i= height-1; i>= 0; i--)
+        {
+            for (j=0; j<i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (k=i; k<=height-1; k++)
+            {
+                System.out.print("8" + " ");
+            }
+            System.out.println(" ");
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -1,7 +1,25 @@
 package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
-    public void printRectangleFrom8s(int length, int height){
+    public void printRectangleFrom8s(int length, int height) {
 
+        for (int i = 0; i < length; i++) {
+            System.out.print("8");
+        }
+        System.out.println(); // Move to the next line
+
+        // Print the sides and interior of the rectangle
+        for (int i = 2; i < height; i++) {
+            System.out.print("8");
+
+            for (int j = 2; j < length; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("8");
+        }
+        for (int i = 0; i < length; i++) {
+            System.out.print("8");
+        }
+        System.out.println();
     }
 }
